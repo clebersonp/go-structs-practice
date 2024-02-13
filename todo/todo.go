dd1810fc5d8c8ce8f34ca1b7034912babb971745
Cleberson <pauluci.cleberson@gmail.com>
2024-02-13 01:08:08 -0300

    Createad todo package and go file

---
+++ todo/todo.go
@@ -0,0 +1,40 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+// Todo struct
+// `json:"text"` are called 'struct tags' - Metadata and golang feature
+type Todo struct {
+	Text string `json:"text"`
+}
+
+// Display - method with receiver of note struct type
+func (t Todo) Display() {
+	fmt.Println(t.Text)
+}
+
+func (t Todo) Save() error {
+	fileName := "todo.json"
+	// Marshal needs to upper case all field struct that will be saved into json format
+	jsonData, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(fileName, jsonData, 0644)
+}
+
+// New constructor function
+func New(content string) (Todo, error) {
+	// validation
+	if content == "" {
+		return Todo{}, errors.New("invalid input")
+	}
+	return Todo{
+		Text: content,
+	}, nil
+}
